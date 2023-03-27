cd6a5af68aecd4bb79ed05a2a4947cd872062c87
Ruslan <111281649+3iN2@users.noreply.github.com>
2023-03-27 16:31:02 +0300

    Add files via upload

---
+++ SkillBox.HomeWork5.Task2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkillBox.HomeWork5.Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите предложение: ");
+            string inputPhrase = Console.ReadLine();
+            SplitText(inputPhrase);
+        }
+        static string[] SplitText(string inputPhrase)
+        {
+            string[] splitSentence = inputPhrase.Split(' ');
+            ReverseSentence(splitSentence);
+            return splitSentence;
+        }
+        static void ReverseSentence (string[] splitSentence)
+        {
+            for (int i = splitSentence.Length-1; i >= 0; i--)
+            {
+                Console.Write(splitSentence[i]+" ");
+
+            }
+        }
+    }
+}
---
+++ SkillBox.HomeWork5/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkillBox.HomeWork5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          Console.WriteLine("Введите предложение: ");
+          SplitText();
+        }
+         static string[] SplitText()
+         {
+            string[] splitSentence = Console.ReadLine().Split(' ');
+            OutputText(splitSentence);
+            return splitSentence;
+         }
+
+        static void OutputText(string[] splitSentence)
+        {
+         for (int i = 0; i < splitSentence.Length; i++)
+            {
+                Console.WriteLine(splitSentence[i]);
+            }
+        }
+    }
+}
